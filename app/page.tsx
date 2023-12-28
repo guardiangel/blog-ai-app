82dfbe9e8a7d6bcf14bc88a3ba6bb4baac213902
guardiangel <felix.sgq@gmail.com>
2023-12-28 10:23:45 -0500

    Trending Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+import Treding from "./(home)/Treding";
+
 export default function Home() {
-  return <main>hi</main>;
+  return (
+    <main className="px-10 leading-7">
+      <Treding />
+      <div className="md:flex gap-10 mb-5">
+        <div className="basis-3/4">
+          {/* <Tech /> */}
+          {/* <Travel /> */}
+          {/* <Other /> */}
+          <div className="hidden md:block">{/* <Subscrite /> */}</div>
+        </div>
+        <div className="basis-1/4">{/* <Sidebar /> */}</div>
+      </div>
+    </main>
+  );
 }
